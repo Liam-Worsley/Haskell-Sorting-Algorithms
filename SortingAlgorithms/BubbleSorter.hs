539873c4e4b0ace9b92a6bbf520aead690cbd651
Liam Worsley <lworsley@trinity.edu>
2024-01-16 12:21:54 -0600

    bubble sort done!

--- SortingAlgorithms/BubbleSorter.hs
+++ SortingAlgorithms/BubbleSorter.hs
@@ -1,24 +1,27 @@
 module SortingAlgorithms.BubbleSorter where
 
 
---starts the bubble sort
+--bubble sort supposedly runs in O(n^2) time and O(1) space
+
+--main function, starts the firstForLoop function
 bubbleSort :: [Int] -> [Int]
-bubbleSort lst = bubbleRecursion lst (length lst - 1)
+bubbleSort lst = firstForLoop lst (length lst - 1)
 
-bubbleRecursion :: [Int] -> Int -> [Int]
-bubbleRecursion lst 0 = lst
-bubbleRecursion lst n = bubbleRecursion (runThrough lst 0 n) (n - 1)
+--outer for loop that continually decreases the size of checking
+firstForLoop :: [Int] -> Int -> [Int]
+firstForLoop lst 0 = lst
+firstForLoop lst n = firstForLoop (secondForLoop lst 0 n) (n - 1)
 
-runThrough :: [Int] -> Int -> Int -> [Int]
-runThrough lst x n
-  | x >= n = lst
-  | goingToSwap lst x = runThrough (swap lst (lst !! x) (lst !! (x+1))) (x+1) n
-  | otherwise = runThrough lst (x+1) n
+--inner for loop that runs through the array and checks for swaps
+secondForLoop :: [Int] -> Int -> Int -> [Int]
+secondForLoop lst x n = if x >= n then lst else if goingToSwap lst x then let newlst = swap lst x 0 in secondForLoop newlst (x+1) n else secondForLoop lst (x+1) n
 
 --checks to see if you should swap the nth and n+1th element in the array
 goingToSwap :: [Int] -> Int -> Bool
 goingToSwap lst n = (lst !! n) > (lst !! (n+1))
 
---performs the swap
+--swaps the ath and a+1th element in the array
 swap :: [Int] -> Int -> Int -> [Int]
-swap lst a b = [if x==a then b else if x==b then a else x | x <- lst]
+swap [] a b = []
+swap [x] a b = [x]
+swap (x:y:xs) a b = if a /= b then [x] ++ swap (y:xs) a (b+1) else y:x:xs 

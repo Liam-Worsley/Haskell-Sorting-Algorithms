057346d747d2553c7cff80811be608bac066f080
Liam Worsley <lworsley@trinity.edu>
2024-01-31 15:39:00 -0600

    merge sort complete, insertion and main file left!

--- SortingAlgorithms/MergeSorter.hs
+++ SortingAlgorithms/MergeSorter.hs
@@ -1,11 +1,21 @@
 module SortingAlgorithms.MergeSorter where
 
-
+--merges the numbers into singletons then recursively merges them back together until the list is sorted.
 mergeSort :: [Int] -> [Int]
 mergeSort lst = mergeBack (mergeApart lst)
 
+--splits the array into singletons
 mergeApart :: [Int] -> [[Int]]
-mergeApart lst = [lst]
+mergeApart lst = [[x] | x <- lst]
 
+--recursively merges the list back into itself
 mergeBack :: [[Int]] -> [Int]
-mergeBack lst = head lst
+mergeBack [lst] = lst
+mergeBack (x:y:xs) = let newlst = xs ++ [mergeTogether x y] in mergeBack newlst
+
+--merging 2 lists into 1
+mergeTogether :: [Int] -> [Int] -> [Int]
+mergeTogether [] [] = []
+mergeTogether lst1 [] = lst1
+mergeTogether [] lst2 = lst2
+mergeTogether (x:xs) (y:ys) = if x < y then [x] ++ (mergeTogether xs (y:ys)) else [y] ++ (mergeTogether (x:xs) ys)
--- SortingAlgorithms/ThanosSorter.hs
+++ SortingAlgorithms/ThanosSorter.hs
@@ -2,22 +2,25 @@ module SortingAlgorithms.ThanosSorter where
 import System.Random
 import System.IO.Unsafe
 
-
-randomNumber :: IO Int
-randomNumber = getStdRandom (randomR (1,2))
-
+--recursively calls itself until the array is sorted, if not it destroys half and checks again
 thanosSort :: [Int] -> [Int]
 thanosSort lst = if isSorted lst then lst else
                  let newRand = randomNumber
                      rand2 = (unsafePerformIO newRand)
                      newlst = destroyHalf lst rand2 0
                  in thanosSort newlst
-                     
+
+--random number generator that returns 1 or 2
+randomNumber :: IO Int
+randomNumber = getStdRandom (randomR (1,2))
+
+--removes every other element of the array, does even or odd based off the random number
 destroyHalf :: [Int] -> Int -> Int -> [Int]
 destroyHalf [] _ _ = []
 destroyHalf (x:xs) 1 iter = if iter `mod` 2 == 0 then destroyHalf xs 1 (iter+1) else x:destroyHalf xs 1 (iter+1)
 destroyHalf (x:xs) 2 iter = if iter `mod` 2 == 0 then x:destroyHalf xs 1 (iter+1) else destroyHalf xs 1 (iter+1)
 
+--checks to see if the list is sorted
 isSorted :: [Int] -> Bool
 isSorted [] = True
 isSorted [x] = True
